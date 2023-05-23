b1c4b59a273151fa8b7a91dbca70264ab7efbe51
franceskora <francesanyakora@gmail.com>
2023-05-22 17:34:42 -0700

    my commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/users"
     user_id = sys.argv[1]
     user_endPoint = "{}/users/{}".format(url,user_id)
-    name = requesta.get(user_endPoint).json().get("name")
+    name = requests.get(user_endPoint).json().get("name")
     task_endPoint = "{}/todos".format(url)
     tasks = requests.get(tasks_endPoint).json()
     user_tasks = [task for task in tasks if task.get("userId") == user_id]
-    completed_tasks = [task for task in user_tasks if task.get("completed")]
+    finished_tasks = [task for task in user_tasks if task.get("completed")]
     print("Employee {} is done with tasks({}/{}):"
             .format(name, len(completed_tasks), len(user_tasks)))
